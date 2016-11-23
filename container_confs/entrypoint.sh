873d87d88a83b70d9da21169c565b2c2ca981330
Joel Gilley <gilleyj@gmail.com>
2016-11-23 20:22:07 +0100

    - entrypoint leveling

--- container_confs/entrypoint.sh
+++ container_confs/entrypoint.sh
@@ -30,8 +30,8 @@ if [ "$1" == "fakesqs" ]; then
 		/usr/bin/fake_sqs -o $HOST_IP -p 4568 --database /var/data/sqs/queues $EXTLOGS --no-daemonize >/dev/null 2>&1
 	fi
 elif [ "$1" == "bash" ] || [ "$1" == "shell" ]; then
-	echo "starting /bin/bash...."
-	/bin/bash --rcfile /etc/bashrc
+	echo "starting /bin/bash with /etc/profile..."
+	/bin/bash --rcfile /etc/profile
 else
 	echo "Running something else"
 	exec "$@"
